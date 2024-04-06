ae2d1517850952a9bedb3f3354e64efc94910477
Babymcnerd <ayden.mcneil@gmail.com>
2024-04-06 16:56:18 -0700

    large ad patterns

--- grandsonwebextension/scripts/content.js
+++ grandsonwebextension/scripts/content.js
@@ -1,53 +1,67 @@
-const patterns = ["para"];
+// const patterns = ["para"];
 
-const content = document.documentElement.innerHTML;
+const url = chrome.runtime.getURL("matches.txt");
 
-matches = [];
-for (var p = 0; p < patterns.length; p++) {
-  i = 0;
-  while (i !== -1) {
-    i = content.indexOf(patterns[p], i);
-    if (i !== -1) {
-      matches.push(i);
-      i += patterns[p].length;
-    }
-  }
+async function getURL(url) {
+  var text = await fetch(url);
+  var output = await text.text();
+  return output;
 }
 
-matching_elements = [];
-for (var i = 0; i < matches.length; i++) {
-  matching_elements.push(null);
-}
-elements_queue = [document.documentElement];
-while (elements_queue.length > 0) {
-  if (elements_queue[0].innerHTML.length == 0) {
-    elements_queue.shift();
-    continue;
-  }
-  i = 0;
-  while (i !== -1) {
-    i = content.indexOf(elements_queue[0].innerHTML, i);
-    if (i !== -1) {
-      for (var m = 0; m < matches.length; m++) {
-        if (
-          i <= matches[m] &&
-          i + elements_queue[0].innerHTML.length >= matches[m]
-        ) {
-          matching_elements[m] = elements_queue[0];
-        }
+var URLoutput = getURL(url).then((Response) => {
+  var URLoutputtext = Response;
+  var patterns = URLoutputtext.split("\r\n");
+  //   console.log(patterns);
+
+  const content = document.documentElement.innerHTML;
+
+  matches = [];
+  for (var p = 0; p < patterns.length; p++) {
+    i = 0;
+    while (i !== -1) {
+      i = content.indexOf(patterns[p], i);
+      if (i !== -1) {
+        matches.push(i);
+        i += patterns[p].length;
       }
-      i += elements_queue[0].innerHTML.length;
     }
   }
-  elements_queue.push(...elements_queue[0].children);
-  elements_queue.shift();
-}
 
-unique_matching_elements = [];
-for (var m = 0; m < matching_elements.length; m++) {
-  if (!unique_matching_elements.includes(matching_elements[m])) {
-    unique_matching_elements.push(matching_elements[m]);
-    matching_elements[m].insertAdjacentHTML("afterbegin", m);
+  matching_elements = [];
+  for (var i = 0; i < matches.length; i++) {
+    matching_elements.push(null);
   }
-}
-console.log(unique_matching_elements);
+  elements_queue = [document.documentElement];
+  while (elements_queue.length > 0) {
+    if (elements_queue[0].innerHTML.length == 0) {
+      elements_queue.shift();
+      continue;
+    }
+    i = 0;
+    while (i !== -1) {
+      i = content.indexOf(elements_queue[0].innerHTML, i);
+      if (i !== -1) {
+        for (var m = 0; m < matches.length; m++) {
+          if (
+            i <= matches[m] &&
+            i + elements_queue[0].innerHTML.length >= matches[m]
+          ) {
+            matching_elements[m] = elements_queue[0];
+          }
+        }
+        i += elements_queue[0].innerHTML.length;
+      }
+    }
+    elements_queue.push(...elements_queue[0].children);
+    elements_queue.shift();
+  }
+
+  unique_matching_elements = [];
+  for (var m = 0; m < matching_elements.length; m++) {
+    if (!unique_matching_elements.includes(matching_elements[m])) {
+      unique_matching_elements.push(matching_elements[m]);
+      matching_elements[m].insertAdjacentHTML("afterbegin", m);
+    }
+  }
+  console.log(unique_matching_elements);
+});
